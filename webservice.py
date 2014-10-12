d6d03080850cc3130e065e0f742bb3dd755c893f
beltzac <beltzac@gmail.com>
2014-10-12 00:25:02 -0300

    Pre processamento(threshold e orientação)

--- webservice.py
+++ webservice.py
@@ -1,13 +1,21 @@
 #!flask/bin/python
 import os
-from flask import Flask, jsonify, request, redirect, url_for, send_from_directory
+import numpy
+import Image
+import string
+from skimage.transform import probabilistic_hough_line, rotate
+from skimage import data
+from skimage.util import img_as_ubyte
+import math
+import timeit
+from skimage import filter
+
+from flask import Flask, jsonify, request
 from werkzeug.utils import secure_filename
-from time import gmtime, strftime
 from Bio.Blast import NCBIWWW
 from Bio.Blast import NCBIXML
 import pytesseract
-import Image
-import string
+import matplotlib.pyplot as plt
 
 
 THIS_FOLDER = os.getcwd()
@@ -19,6 +27,14 @@ app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 
+def angle(ps):
+    x1, y1 = ps[0]
+    x2, y2 = ps[1]
+
+    m = (y2 - y1) / float(x2 - x1)
+
+    return math.atan(m)
+
 
 def blastn(sequencia):
     lista = []
@@ -26,7 +42,7 @@ def blastn(sequencia):
     registros = NCBIXML.parse(resultado)
     registro_blast = registros.next()
     for alinhamento in registro_blast.alignments:
-        #print alinhamento.hsps
+        # print alinhamento.hsps
         for hsp in alinhamento.hsps:
             # print "****Alinhamento****"
             reg = {
@@ -39,7 +55,7 @@ def blastn(sequencia):
                 'query-c': hsp.sbjct[:50]
             }
             lista.append(reg)
-            #print reg
+            # print reg
             # print "----------------------------------------------"
 
     return lista[:10]
@@ -48,16 +64,6 @@ def blastn(sequencia):
 def allowed_file(filename):
     return '.' in filename and \
            filename.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS
-
-
-@app.route('/upload/<path:filename>')
-def send_upload(filename):
-    return send_from_directory(UPLOAD_FOLDER, filename)
-
-
-@app.route('/output/<path:filename>')
-def send_output(filename):
-    return send_from_directory(OUTPUT_FOLDER, filename)
 
 
 @app.route('/web/', methods=['GET', 'POST'])
@@ -72,20 +78,111 @@ def site_input():
             filename = secure_filename(file.filename)
             full_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
             file.save(full_path)
-            imagemDNA = Image.open(full_path);
 
-            texto = pytesseract.image_to_string(imagemDNA,config='-psm 3')
+            # imagemDNA = Image.open(full_path)
+            #
+            # texto = pytesseract.image_to_string(imagemDNA)
+            # print(texto)
+            # print ('1' * 30)
+
+            # local threshold
+
+
+            img = img_as_ubyte(data.imread(full_path, True))
+
+            start_time = timeit.default_timer()
+            threshold_local = filter.threshold_adaptive(img, 201)
+            elapsed = timeit.default_timer() - start_time
+
+            print('threshold_local:', elapsed)
+
+            # texto = pytesseract.image_to_string(Image.fromarray(numpy.uint8(threshold_local)))
+            # print(texto)
+            # print ('2' * 30)
+
+
+            # edge
+
+            start_time = timeit.default_timer()
+            edges2 = filter.canny(threshold_local, sigma=2)
+            elapsed = timeit.default_timer() - start_time
+            print('canny:', elapsed)
+
+            # Deskew
+
+            start_time = timeit.default_timer()
+            theta = numpy.linspace(1.39, 1.74, 30)
+
+            lines = probabilistic_hough_line(edges2, line_gap=50, line_length=600, theta=theta)
+            print(len(lines))
+
+            angs = [angle(ps) for ps in lines]
+            rot = numpy.mean(angs)
+            ang = math.degrees(rot)
+            print (ang)
+            elapsed = timeit.default_timer() - start_time
+            print('deskew:', elapsed)
+
+            final = rotate(threshold_local, ang)
+
+            imgplot = plt.imshow(final, cmap=plt.cm.gray)
+
+            texto = pytesseract.image_to_string(Image.fromarray(numpy.uint8(final)))
             print(texto)
             print ('-' * 30)
-            all = string.maketrans('','')
-            strdna = all.translate(all,'acgtACGT')
-            texto2 = texto.translate(all,strdna).upper()
+
+            if (True):
+                # results threshold
+
+                fig, ax = plt.subplots(1, 2, figsize=(8, 5))
+                ax1, ax2, = ax.ravel()
+
+                fig.colorbar(ax1.imshow(img, cmap=plt.cm.gray),
+                             ax=ax1, orientation='horizontal')
+                ax1.set_title('Original')
+                ax1.axis('off')
+
+                fig.colorbar(ax2.imshow(threshold_local, cmap=plt.cm.gray),
+                             ax=ax2, orientation='horizontal')
+                ax2.set_title('Local threshold (radius=%d)' % 101)
+                ax2.axis('off')
+
+                plt.show()
+
+
+
+                # results lines
+                fig2, ax = plt.subplots(1, 3, figsize=(8, 3))
+
+                ax[0].imshow(threshold_local, cmap=plt.cm.gray)
+                ax[0].set_title('Input image')
+                ax[0].axis('image')
+
+                ax[1].imshow(edges2, cmap=plt.cm.gray)
+                ax[1].set_title('Canny edges')
+                ax[1].axis('image')
+
+                ax[2].imshow(edges2, cmap=plt.cm.gray)
+
+                for line in lines:
+                    p0, p1 = line
+                    ax[2].plot((p0[0], p1[0]), (p0[1], p1[1]))
+
+                ax[2].set_title('Probabilistic Hough')
+                ax[2].axis('image')
+                plt.show()
+
+            all = string.maketrans('', '')
+            strdna = all.translate(all, 'acgtACGT')
+            texto2 = texto.translate(all, strdna).upper()
             print (texto2)
-            # texto , tempo = octave.runOCR(full_path)
-            #texto = 'ATGGAAAACTTTTGGCAGGCCTGCTCTCAAAAACTTGAGCAGGAGCTGACACCCCAGCAATACAGCGCCTGGATCAAGCCCCTGGTGCCGCTCGACTACGAAGACGGGCTGCTGCGCGTGGCCGCGCCCAATCGGTTCAAGCTGGACTGGGTCAAGACCCAGTTCGCCAACCGCATCACCGCGCTGGCCTGCGAGTACTGGGACGCGCCCACCGAGGTGCAATTCGTGCTCGACCCGCGTGGCAACCAGGGCCGTCGTCCGGCGGCGGCCGCCGCGGCCGGCAATGGCGCCAGCGGTCTGGGTTTGCCCAATCACGAGCAATTGCACCTGGACCCCGAACCGGCCCAGCCGGTGCGCGCGGTCGCGCCGCGCCAGGAGCAGTCGCGTATCAACCCGGTGTTGACCTTCGACAATCTGGTGACGGGTAAGGCCAACCAGCTTGCCCGCGCAGCCGCCACCCAGGTGGCCAACAACCCCGGCACGTCCTACAACCCGCTGTTCCTGTACGGCGGCGTCGGCCTGGGTAAGACCCACATCATCCACGCCATCGGCAACCAGGTGCTTGTGGATAACCCGGGCGCGAAAATCCGCTACATCCACGCCG'
+
             if len(texto2) > 0:
+                start_time = timeit.default_timer()
                 temp = blastn(texto2)
-            erro = 0
+                elapsed = timeit.default_timer() - start_time
+                print('blast:', elapsed)
+                erro = 0
         else:
             erro = 1
 
@@ -103,22 +200,6 @@ def site_input():
          <input type=submit value=Upload>
     </form>
     '''
-
-
-@app.route('/', methods=['GET', 'POST'])
-def app_input():
-    if request.method == 'POST':
-        file = request.files['file']
-        if file and allowed_file(file.filename):
-            filename = secure_filename(file.filename)
-            full_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
-            file.save(full_path)
-
-            return " "
-
-    json = jsonify({'success': 'false',
-                    'server_time': strftime("%Y-%m-%d %H:%M:%S", gmtime())})
-    return json
 
 
 if __name__ == '__main__':
