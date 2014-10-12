68772c5992e06ecbe805f5a63e2190785c8b23d7
beltzac <beltzac@gmail.com>
2014-10-12 20:14:00 -0300

    Melhorando os graficos

--- webservice.py
+++ webservice.py
@@ -79,34 +79,20 @@ def site_input():
             full_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
             file.save(full_path)
 
-            # imagemDNA = Image.open(full_path)
-            #
-            # texto = pytesseract.image_to_string(imagemDNA)
-            # print(texto)
-            # print ('1' * 30)
-
-            # local threshold
-
-
             img = img_as_ubyte(data.imread(full_path, True))
 
             start_time = timeit.default_timer()
             threshold_local = filter.threshold_adaptive(img, 201)
             elapsed = timeit.default_timer() - start_time
 
-            print('threshold_local:', elapsed)
-
-            # texto = pytesseract.image_to_string(Image.fromarray(numpy.uint8(threshold_local)))
-            # print(texto)
-            # print ('2' * 30)
-
+            print("threshold_local:", elapsed)
 
             # edge
 
             start_time = timeit.default_timer()
             edges2 = filter.canny(threshold_local, sigma=2)
             elapsed = timeit.default_timer() - start_time
-            print('canny:', elapsed)
+            print("canny:", elapsed)
 
             # Deskew
 
@@ -114,68 +100,62 @@ def site_input():
             theta = numpy.linspace(1.39, 1.74, 30)
 
             lines = probabilistic_hough_line(edges2, line_gap=50, line_length=600, theta=theta)
-            print(len(lines))
+            print("Lines:", len(lines))
 
             angs = [angle(ps) for ps in lines]
             rot = numpy.mean(angs)
             ang = math.degrees(rot)
-            print (ang)
             elapsed = timeit.default_timer() - start_time
-            print('deskew:', elapsed)
+            print("deskew:", elapsed)
+            print("Angle:", ang, "Degres")
 
             final = rotate(threshold_local, ang)
-
-            imgplot = plt.imshow(final, cmap=plt.cm.gray)
-
+            start_time = timeit.default_timer()
             texto = pytesseract.image_to_string(Image.fromarray(numpy.uint8(final)))
+            elapsed = timeit.default_timer() - start_time
+            print("OCR:", elapsed)
+
             print(texto)
             print ('-' * 30)
-
-            if (True):
-                # results threshold
-
-                fig, ax = plt.subplots(1, 2, figsize=(8, 5))
-                ax1, ax2, = ax.ravel()
-
-                fig.colorbar(ax1.imshow(img, cmap=plt.cm.gray),
-                             ax=ax1, orientation='horizontal')
-                ax1.set_title('Original')
-                ax1.axis('off')
-
-                fig.colorbar(ax2.imshow(threshold_local, cmap=plt.cm.gray),
-                             ax=ax2, orientation='horizontal')
-                ax2.set_title('Local threshold (radius=%d)' % 101)
-                ax2.axis('off')
-
-                plt.show()
-
-
-
-                # results lines
-                fig2, ax = plt.subplots(1, 3, figsize=(8, 3))
-
-                ax[0].imshow(threshold_local, cmap=plt.cm.gray)
-                ax[0].set_title('Input image')
-                ax[0].axis('image')
-
-                ax[1].imshow(edges2, cmap=plt.cm.gray)
-                ax[1].set_title('Canny edges')
-                ax[1].axis('image')
-
-                ax[2].imshow(edges2, cmap=plt.cm.gray)
-
-                for line in lines:
-                    p0, p1 = line
-                    ax[2].plot((p0[0], p1[0]), (p0[1], p1[1]))
-
-                ax[2].set_title('Probabilistic Hough')
-                ax[2].axis('image')
-                plt.show()
 
             all = string.maketrans('', '')
             strdna = all.translate(all, 'acgtACGT')
             texto2 = texto.translate(all, strdna).upper()
             print (texto2)
+
+            if (True):
+
+                # results
+                fig, ax = plt.subplots(2, 3, figsize=(8, 3))
+
+                ax0, ax1, ax2, ax3, ax4, ax5 = ax.ravel()
+
+                ax0.imshow(img, cmap=plt.cm.gray)
+                ax0.set_title('Original')
+                ax0.axis('image')
+
+                ax1.imshow(threshold_local, cmap=plt.cm.gray)
+                ax1.set_title('Local threshold (radius=%d)' % 101)
+                ax1.axis('image')
+
+                ax2.imshow(edges2, cmap=plt.cm.gray)
+                ax2.set_title('Canny edges')
+                ax2.axis('image')
+
+                ax3.imshow(edges2, cmap=plt.cm.gray)
+
+                for line in lines:
+                    p0, p1 = line
+                    ax3.plot((p0[0], p1[0]), (p0[1], p1[1]))
+
+                ax3.set_title('Probabilistic Hough')
+                ax3.axis('image')
+
+                ax4.imshow(final, cmap=plt.cm.gray)
+                ax4.set_title('Deskew')
+                ax4.axis('image')
+
+                plt.show()
 
             if len(texto2) > 0:
                 start_time = timeit.default_timer()
